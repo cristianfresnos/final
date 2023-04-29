7bd205d843071daa2fee749bd1c5c1fa48074b1e
cristianfresnos <cristian.fresnos@uniminuto.edu.co>
2023-04-29 11:32:11 -0500

    Agreado archivo principal de php con creación mia

---
+++ EjercicioBD.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>City Parking - Brand</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/styles.min.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/estilos.css">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+    <script src="assets/js/script.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    
+</head>
+
+
+<body>
+  <!-- <style>
+		img {
+			margin-left: 50px;
+		}
+	  </style> -->
+    <nav class="navbar navbar-light navbar-expand-lg fixed-top" id="mainNav">
+        <div class="container"><img src="images/city.JPG" alt="logo" style="position: absolute;"><button data-bs-toggle="collapse"
+                data-bs-target="#navbarResponsive" class="navbar-toggler" aria-controls="navbarResponsive"
+                aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a class="nav-link" href="/EjercicioBD.php">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/about.php">About us</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/contact.php">Contact us</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/contact.php">Blog Post</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <header class="masthead" style="background-image: url('assets/img/home1-bg.jpg');">
+        <div class="overlay"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 col-lg-8 mx-auto position-relative">
+                    <div class="site-heading">
+                        <h1>City Parking</h1><span class="subheading">A Blog Theme by Start Bootstrap</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-lg-8">
+                <div class="post-preview"><a href="#">
+                        <h2 class="post-title">Man must explore, and this is exploration at its greatest</h2>
+                        <h3 class="post-subtitle">Problems look mighty small from 150 miles up</h3>
+                    </a>
+                    <p class="post-meta">Posted by&nbsp;<a href="#">Start Bootstrap on September 24, 2018</a></p>
+                </div>
+                <hr>
+                <div class="post-preview"><a href="#">
+                        <h2 class="post-title">I believe every human has a finite number of heartbeats. I don't intend
+                            to waste any of mine.</h2>
+                    </a>
+                    <p class="post-meta">Posted by&nbsp;<a href="#">Start Bootstrap on September 18, 2018</a></p>
+                </div>
+                <hr>
+                <div class="post-preview"><a href="#">
+                        <h2 class="post-title"> Science has not yet mastered prophecy</h2>
+                        <h3 class="post-subtitle">We predict too much for the next year and yet far too little for the
+                            next ten.</h3>
+                    </a>
+                    <p class="post-meta">Posted by&nbsp;<a href="#">Start Bootstrap on August 24, 2018</a></p>
+                </div>
+                <hr>
+                <div class="post-preview"><a href="#">
+                        <h2 class="post-title">Failure is not an option</h2>
+                        <h3 class="post-subtitle">Many say exploration is part of our destiny, but it’s actually our
+                            duty to future generations.</h3>
+                    </a>
+                    <p class="post-meta">Posted by&nbsp;<a href="#">Start Bootstrap on July 8, 2018</a></p>
+                </div>
+                <hr>
+                <div class="clearfix"><button class="btn btn-primary float-end" type="button">Older
+                        Posts&nbsp;⇒</button></div>
+            </div>
+        </div>
+    </div>
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 col-lg-8 mx-auto">
+                    <ul class="list-inline text-center">
+                        <li class="list-inline-item"><span class="fa-stack fa-lg"><i
+                                    class="fa fa-circle fa-stack-2x"></i><i
+                                    class="fa fa-twitter fa-stack-1x fa-inverse"></i></span></li>
+                        <li class="list-inline-item"><span class="fa-stack fa-lg"><i
+                                    class="fa fa-circle fa-stack-2x"></i><i
+                                    class="fa fa-facebook fa-stack-1x fa-inverse"></i></span></li>
+                        <li class="list-inline-item"><span class="fa-stack fa-lg"><i
+                                    class="fa fa-circle fa-stack-2x"></i><i
+                                    class="fa fa-github fa-stack-1x fa-inverse"></i></span></li>
+                    </ul>
+                    <p class="text-muted copyright">Copyright&nbsp;©&nbsp;Brand 2023</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+    
+    
+</body>
+
+</html>
